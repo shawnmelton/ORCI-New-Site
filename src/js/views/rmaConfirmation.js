a691ff077da20ef138ff233a8374c52b29b66a4b
Shawn Melton <shawn.a.melton@gmail.com>
2013-10-17 22:41:44 -0400

    Polishing off form and overlay confirmation.

--- src/js/views/rmaConfirmation.js
+++ src/js/views/rmaConfirmation.js
@@ -1,37 +1,43 @@
 define(['jquery', 'backbone', 'templates/html.jst'], function($, Backbone, htmlJST){
-        var rmaConfirmationView = Backbone.View.extend({
-            el: "body",
-            overlayBG: null,
-            overlayContent: null,
+    var rmaConfirmationView = Backbone.View.extend({
+        el: "body",
+        overlayBG: null,
+        overlayContent: null,
+        rmaRequest: null,
 
-            events: {
-                'click #confirm-rma-request': 'onConfirmButtonClick',
-                'click #cancel-rma-request': 'onCancelButtonClick',
-                'click #ack-overlay-bg': 'onCancelButtonClick'
-            },
+        events: {
+            'click #confirm-rma-request': 'onConfirmButtonClick',
+            'click #cancel-rma-request': 'onCancelButtonClick',
+            'click #ack-overlay-bg': 'onCancelButtonClick'
+        },
 
-            closeOverlay: function() {
-                this.overlayBG.remove();
-                this.overlayContent.remove();
-            },
+        closeOverlay: function() {
+            this.overlayBG.remove();
+            this.overlayContent.remove();
+            this.rmaRequest = null;
+        },
 
-            onCancelButtonClick: function() {
-                this.closeOverlay();
-            },
+        onCancelButtonClick: function() {
+            this.closeOverlay();
+        },
 
-            onConfirmButtonClick: function() {
-                this.closeOverlay();
-            },
-
-            render: function(){
-                this.$el
-                    .append(JST['src/js/templates/rmaConfirmation.html']({}));
-
-                this.overlayBG = $("#ack-overlay-bg");
-                this.overlayContent = $("#ack-overlay");
+        onConfirmButtonClick: function() {
+            if(this.rmaRequest !== null) {
+                this.rmaRequest.onFormRequestConfirmation();
             }
-        });
-        
-        return new rmaConfirmationView();
-    }
-);
+
+            this.closeOverlay();
+        },
+
+        render: function(request){
+            this.$el
+                .append(JST['src/js/templates/rmaConfirmation.html']({}));
+
+            this.overlayBG = $("#ack-overlay-bg");
+            this.overlayContent = $("#ack-overlay");
+            this.rmaRequest = request;
+        }
+    });
+    
+    return new rmaConfirmationView();
+});
--- src/js/views/workOrderRequestForm.js
+++ src/js/views/workOrderRequestForm.js
@@ -1,206 +1,221 @@
 define(['jquery', 'backbone', 'templates/html.jst', 'views/rmaConfirmation'], function($, Backbone, htmlJST, rmaConfirmationView){
-        var workOrderRequestFormView = Backbone.View.extend({
-            el: "#content",
-            form: null,
-            productCount: 1,
-            nonusFieldsChecked: false,
+    var workOrderRequestFormView = Backbone.View.extend({
+        el: "#content",
+        form: null,
+        productCount: 1,
+        nonusFieldsChecked: false,
 
-            events: {
-                'click #submit': 'onSubmitButtonClick',
-                'click #reset': 'onResetButtonClick',
-                'click #add-product': 'onAddProductClick',
-                'click #located-in-nonus': 'onLocationCheckboxClick',
-                'submit #rma-request-form': 'onFormSubmission'
-            },
+        events: {
+            'click #submit': 'onSubmitButtonClick',
+            'click #reset': 'onResetButtonClick',
+            'click #add-product': 'onAddProductClick',
+            'click #located-in-nonus': 'onLocationCheckboxClick',
+            'submit #rma-request-form': 'onFormSubmission'
+        },
 
-            /**
-             * Remove the error styling from fields.
-             */
-            clearFormErrors: function() {
-                if(this.form !== null) {
-                    this.form.find("input, select").removeClass("error");
-                    this.form.find("label")
-                        .removeClass("empty")
-                        .removeClass('invalid-email')
-                        .removeClass('invalid-zip');
+        /**
+         * Remove the error styling from fields.
+         */
+        clearFormErrors: function() {
+            if(this.form !== null) {
+                this.form.find("input, select").removeClass("error");
+                this.form.find("label")
+                    .removeClass("empty")
+                    .removeClass('invalid-email')
+                    .removeClass('invalid-zip');
+            }
+        },
+
+        /**
+         * Handle what happens when the "Add Product" button is clicked.
+         */
+        onAddProductClick: function() {
+            $("#products-to-return").append(
+                JST['src/js/templates/productReturnFields.html']({
+                    cnt: this.productCount
+                })
+            );
+
+            this.productCount++;
+        },
+
+        /**
+         * Handle what happens the user has confirmed that they want to 
+         * submit this RMA request
+         */
+        onFormRequestConfirmation: function() {
+            this.form.submit();
+        },
+
+        /**
+         * Catch the form submission to check for validation.
+         */
+        onFormSubmission: function(event) {
+            if(this.form !== null && this.validSubmission()) {
+                this.form.append('<input type="hidden" name="rma-request-form-v" value="1">');
+                this.form.attr("action", "/content/orci-forms.php");
+            } else {
+                event.preventDefault();
+            }
+        },
+
+        /**
+         * Handle what happens when the "I am located outside of the United States" button is clicked.
+         */
+        onLocationCheckboxClick: function() {
+            var fieldsToShow,
+                fieldsToHide;
+
+            this.nonusFieldsChecked = !this.nonusFieldsChecked;
+
+            if(this.nonusFieldsChecked) { // Show Non-US fields
+                fieldsToHide = $("#us-fields");
+                fieldsToShow = $("#non-us-fields");
+            } else { // Show US fields
+                fieldsToHide = $("#non-us-fields");
+                fieldsToShow = $("#us-fields");
+            }
+
+            fieldsToHide.slideUp(function() {
+                fieldsToShow.slideDown();
+            });
+
+            fieldsToShow.find("label").each(function() {
+                if(!$(this).hasClass("req")) {
+                    $(this).addClass("req");
                 }
-            },
+            });
 
-            /**
-             * Handle what happens when the "Add Product" button is clicked.
-             */
-            onAddProductClick: function() {
-                $("#products-to-return").append(
-                    JST['src/js/templates/productReturnFields.html']({
-                        cnt: this.productCount
-                    })
-                );
+            fieldsToHide.find("label").each(function() {
+                if($(this).hasClass("req")) {
+                    $(this).removeClass("req");
+                }
+            });
+        },
 
-                this.productCount++;
-            },
+        /**
+         * Reset all of the entered values for the form.
+         */
+        onResetButtonClick: function() {
+            this.clearFormErrors();
+            if(this.form !== null) {
+                this.form.find("input, select").each(function() {
+                    $(this).val("");
+                });
+            }
 
-            /**
-             * Catch the form submission to check for validation.
-             */
-            onFormSubmission: function(event) {
-                if(this.form !== null && this.validSubmission()) {
-                    this.form.append('<input type="hidden" name="rma-request-form-v" value="1">');
-                    this.form.attr("action", "/content/orci-forms.php");
+            // Remove the rows after the header and first row.
+            var rowCount = 1;
+            $("#products-to-return tr").each(function() {
+                if(rowCount > 1) {
+                    $(this).remove();
+                }
+
+                rowCount++;
+            });
+
+            this.onAddProductClick(); // Add back first row again.
+        },
+
+        /**
+         * Fire the submission for this form if everything validates.
+         */
+        onSubmitButtonClick: function() {
+            if(this.form !== null && this.validSubmission()) {
+                window.scrollTo(0, 0);
+                rmaConfirmationView.render(this);
+            }
+        },
+
+        render: function(){
+            this.$el
+                .html(JST['src/js/templates/workOrderRequestForm.html']({}))
+                .parent().removeClass("home");
+
+            this.form = $("#rma-request-form");
+            this.onAddProductClick(); // Add first row of table.
+        },
+
+        /**
+         * Display the form field error.
+         */
+        showFieldError: function(field) {
+            field.addClass("error");
+
+            if(field.val() === "") {
+                field.prev().addClass("empty");
+            } else if(field.attr("id") === "email") {
+                field.prev().addClass("invalid-email");
+            } else if(field.attr("id") === "zip-code") {
+                field.prev().addClass("invalid-zip");
+            } 
+        },
+
+        /**
+         * Make sure the user has filled out the entire row of information for
+         * each product row that has a single value.  Also, make sure that 
+         * at least one product has been entered.
+         */
+        validateProducts: function() {
+            var noErrors = true;
+            var row = 1;
+
+            $("#products-to-return tr").each(function() {
+                var fieldHasValue = false;
+                var notAllFieldsHaveValues = false;
+
+                $(this).find("input, select").each(function() {
+                    if(!fieldHasValue && $(this).val() !== "") {
+                        fieldHasValue = true;
+                    } else if(notAllFieldsHaveValues === false && $(this).val() === "") {
+                        notAllFieldsHaveValues = true;
+                    }
+                });
+
+                console.log((!fieldHasValue && row === 1));
+
+                // First row must have a field with a value.
+                if((fieldHasValue && notAllFieldsHaveValues) || (!fieldHasValue && row === 2)) {
+                    noErrors = false;
+                    $(this).addClass("errorRow");
                 } else {
-                    event.preventDefault();
-                }
-            },
-
-            /**
-             * Handle what happens when the "I am located outside of the United States" button is clicked.
-             */
-            onLocationCheckboxClick: function() {
-                var fieldsToShow,
-                    fieldsToHide;
-
-                this.nonusFieldsChecked = !this.nonusFieldsChecked;
-
-                if(this.nonusFieldsChecked) { // Show Non-US fields
-                    fieldsToHide = $("#us-fields");
-                    fieldsToShow = $("#non-us-fields");
-                } else { // Show US fields
-                    fieldsToHide = $("#non-us-fields");
-                    fieldsToShow = $("#us-fields");
+                    $(this).removeClass("errorRow");
                 }
 
-                fieldsToHide.slideUp(function() {
-                    fieldsToShow.slideDown();
-                });
+                row++;
+            });
 
-                fieldsToShow.find("label").each(function() {
-                    if(!$(this).hasClass("req")) {
-                        $(this).addClass("req");
-                    }
-                });
+            return noErrors;
+        },
 
-                fieldsToHide.find("label").each(function() {
-                    if($(this).hasClass("req")) {
-                        $(this).removeClass("req");
-                    }
-                });
-            },
+        /**
+         * Ensure form submission is valid.
+         * @return boolean
+         */
+        validSubmission: function() {
+            var noErrors = true;
+            this.clearFormErrors();
 
-            /**
-             * Reset all of the entered values for the form.
-             */
-            onResetButtonClick: function() {
-                this.clearFormErrors();
-                if(this.form !== null) {
-                    this.form.find("input, select").each(function() {
-                        $(this).val("");
-                    });
-                }
+            if(this.form === null) { // Ran into an error.
+                return false;
+            }
 
-                // Remove the rows after the header and first row.
-                var rowCount = 1;
-                $("#products-to-return tr").each(function() {
-                    if(rowCount > 2) {
-                        $(this).remove();
-                    }
-
-                    rowCount++;
-                });
-            },
-
-            /**
-             * Fire the submission for this form if everything validates.
-             */
-            onSubmitButtonClick: function() {
-                if(this.form !== null && this.validSubmission()) {
-                    //this.form.submit();
-                    rmaConfirmationView.render();
-                }
-            },
-
-            render: function(){
-                this.$el
-                    .html(JST['src/js/templates/workOrderRequestForm.html']({}))
-                    .parent().removeClass("home");
-
-                this.form = $("#rma-request-form");
-                this.onAddProductClick(); // Add first row of table.
-
-                // TODO - Remove
-                rmaConfirmationView.render();
-            },
-
-            /**
-             * Display the form field error.
-             */
-            showFieldError: function(field) {
-                field.addClass("error");
-
-                if(field.val() === "") {
-                    field.prev().addClass("empty");
-                } else if(field.attr("id") === "email") {
-                    field.prev().addClass("invalid-email");
-                } else if(field.attr("id") === "zip-code") {
-                    field.prev().addClass("invalid-zip");
-                } 
-            },
-
-            /**
-             * Make sure the user has filled out the entire row of information.
-             */
-            validateProducts: function() {
-                var noErrors = true;
-                $("#products-to-return tr").each(function() {
-                    var fieldHasValue = false;
-                    var notAllFieldsHaveValues = false;
-
-                    $(this).find("input, select").each(function() {
-                        if(!fieldHasValue && $(this).val() !== "") {
-                            fieldHasValue = true;
-                        } else if(notAllFieldsHaveValues === false && $(this).val() === "") {
-                            notAllFieldsHaveValues = true;
-                        }
-                    });
-
-                    if(fieldHasValue && notAllFieldsHaveValues) {
-                        noErrors = false;
-                        $(this).addClass("errorRow");
-                    } else {
-                        $(this).removeClass("errorRow");
-                    }
-                });
-
-                return noErrors;
-            },
-
-            /**
-             * Ensure form submission is valid.
-             * @return boolean
-             */
-            validSubmission: function() {
-                var noErrors = true;
-                this.clearFormErrors();
-
-                if(this.form === null) { // Ran into an error.
-                    return false;
-                }
-
-                var _this = this;
-                this.form.find("label.req").each(function() {
-                    var field = $(this).next();
-                    if(field.val() === "" || (field.attr("id") === "email" && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(field.val())) || (field.attr("id") === "zip-code" && !/^\d{5}(?:[-\s]\d{4})?$/i.test(field.val()))) {
-                        _this.showFieldError(field);
-                        noErrors = false;
-                    }
-                });
-
-                if(!this.validateProducts()) {
+            var _this = this;
+            this.form.find("label.req").each(function() {
+                var field = $(this).next();
+                if(field.val() === "" || (field.attr("id") === "email" && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(field.val())) || (field.attr("id") === "zip-code" && !/^\d{5}(?:[-\s]\d{4})?$/i.test(field.val()))) {
+                    _this.showFieldError(field);
                     noErrors = false;
                 }
+            });
 
-                return noErrors;
+            if(!this.validateProducts()) {
+                noErrors = false;
             }
-        });
-        
-        return new workOrderRequestFormView();
-    }
-);
+
+            return noErrors;
+        }
+    });
+    
+    return new workOrderRequestFormView();
+});
